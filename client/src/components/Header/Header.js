64b3e920e2e95bd9ce6154a92627e9f8d924e5cb
Austin <ataraxia@Austins-MacBook-Air.local>
2023-06-13 16:20:15 -0300

    Added complete structure for header and basic style on homepage.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,21 +1,21 @@
 import { NavLink } from "react-router-dom";
 
-import headerImage from "../../assets/header.png";
+import headerImage from "../../assets/half-header.png";
 import profilePic from "../../assets/austin-profile-pic.png"
 
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <div>
-      <div className="header__container">
+    <div className="header__container">
+      <div>
         <img className="header__image" src={headerImage} />
       </div>
-      <nav>
+      <nav className="header__nav">
           <ul className="header__ul">
-            <li className="header__li"><NavLink className="header__navlink">SPEAK</NavLink></li>
-            <li className="header__li"><NavLink className="header__navlink">STUDY</NavLink></li>
-            <li className="header__li"><NavLink><img className="header__profile-img" src={profilePic}/></NavLink></li>
+            <li className="header__li"><NavLink className="header__navlink"><button><h4>SPEAK</h4></button></NavLink></li>
+            <li className="header__li"><NavLink className="header__navlink"><button><h4>STUDY</h4></button></NavLink></li>
+            <li className="header__li header__li--image"><NavLink><img className="header__profile-img" src={profilePic}/></NavLink></li>
           </ul>
         </nav>
     </div>
