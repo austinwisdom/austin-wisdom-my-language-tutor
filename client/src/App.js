1e9d41728629c07eeb0fef1fdc1d8767ea147365
Austin <ataraxia@Austins-MacBook-Air.local>
2023-06-12 23:00:59 -0300

    Added initial routes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.scss';
 
@@ -8,6 +8,16 @@ function App() {
       <header className="App-header">
         <h1>Greatness starts here.</h1>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+          <Route path="/my-profile" />
+          <Route path="/my-tutor" />
+          <Route path="/my-tutor/conversation/:id" />
+          <Route path="/study-space" />
+          <Route path="/community" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
