372ec9cb65f38d92dbc77cd9dc26a6839937de26
Austin <ataraxia@Austins-MacBook-Air.local>
2023-06-19 10:17:37 -0300

    Imported knex on myTutor.js.

--- server/controllers/myTutor-controller.js
+++ server/controllers/myTutor-controller.js
@@ -1,6 +1,8 @@
 const OpenAI = require('openai')
 const { Configuration, OpenAIApi } = OpenAI
 const express = require('express')
+const knex = require('knex')(require('../knexfile'))
+
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
@@ -23,23 +25,31 @@ const getOpenAIResponse = async (req, res) => {
     let languageToSend = req.params.language
     let topicToSend = req.params.topic
 
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `Pretend you're a barista at a coffee shop, you're helping Me to complete their order. Here are your objectives for the conversation:\n1. Find out what they want to drink, and what size they want.\n2. Find out what they want to eat.\n3. Ask if there's anything else you can get for them today.\n4. Tell them the total price for their order (like $5.87 for example).\n5. Thank them for their order, tell them to have a great day.\n\nAfter these objectives have been completed, the conversation is marked as concluded.\n\nBegin conversation below:\nBarista: Hi there, what can I get started for you today?\n\nMe: ${message}`,
-        temperature: 1,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        stop: ["Me"],
-      });
-    if (response.data.choices[0].text) {
-        res.json({
-            message: response.data.choices[0].text
+    await knex("conversations_en")
+        .select("prompt")
+        .where({id: topicToSend})
+        .then(async (data) => {
+            console.log(topicToSend)
+            const response = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: `Pretend you're a barista at a coffee shop, you're helping Me to complete their order. Here are your objectives for the conversation:\n1. Find out what they want to drink, and what size they want.\n2. Find out what they want to eat.\n3. Ask if there's anything else you can get for them today.\n4. Tell them the total price for their order (like $5.87 for example).\n5. Thank them for their order, tell them to have a great day.\n\nAfter these objectives have been completed, the conversation is marked as concluded.\n\nBegin conversation below:\nBarista: Hi there, what can I get started for you today?\n\nMe: ${message}`,
+                temperature: 1,
+                max_tokens: 256,
+                top_p: 1,
+                frequency_penalty: 0,
+                presence_penalty: 0,
+                stop: ["Me"],
+              });
+            if (response.data.choices[0].text) {
+                res.json({
+                    message: response.data.choices[0].text
+                })
+            } else {
+                res.json("Unable to get AI response")
+            }
         })
-    } else {
-        res.json("Unable to get AI response")
-    }
+
+    
 
     
 }
