a3968cf2028289dfa2b8a0e091f8d9d2a4179fb9
Austin <ataraxia@Austins-MacBook-Air.local>
2023-06-22 17:07:19 -0300

    Added promise (thanks Patrickgit status) to speechSynth file and controller returns the audio file.

--- server/controllers/speechSynthesis-controller.js
+++ server/controllers/speechSynthesis-controller.js
@@ -1,9 +1,9 @@
 const {synthesizeSpeech} = require("../utilities/SpeechSynthesis")
 
-const getSynthesizedSpeech = (req, res) => {
+const getSynthesizedSpeech = async (req, res) => {
     const textToSend = req.body.textToSynthesize
-    synthesizeSpeech(textToSend)
-    res.status(200).json(`This will be synthed: ${textToSend}`)
+    const file = await synthesizeSpeech(textToSend)
+    res.status(200).json(`Here's the file: ${file}`)
 }
 
 module.exports = {
--- server/utilities/SpeechSynthesis.js
+++ server/utilities/SpeechSynthesis.js
@@ -7,35 +7,40 @@ const synthesizeSpeech = (text) => {
     require("dotenv").config()
     const { SPEECH_KEY, SPEECH_REGION } = process.env
 
-    var audioFile = "YourAudioFile.wav";
-    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    return new Promise((resolve, reject) => { 
+      var audioFile = `${Date.now()}.wav`;
+      // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+      
+      const speechConfig = sdk.SpeechConfig.fromSubscription(SPEECH_KEY, SPEECH_REGION);
+      const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+  
+      // The language of the voice that speaks.
+      speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural"; 
+  
+      // Create the speech synthesizer.
+      var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
+  
+      synthesizer.speakTextAsync(text,
+        function (result) {
+      if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+        console.log("synthesis finished.");
+        resolve(audioFile)
+      } else {
+        console.error("Speech synthesis canceled, " + result.errorDetails +
+            "\nDid you set the speech resource key and region values?");
+      }
     
-    const speechConfig = sdk.SpeechConfig.fromSubscription(SPEECH_KEY, SPEECH_REGION);
-    const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
+      synthesizer.close();
+      synthesizer = null;
+    },
+        function (err) {
+      console.trace("err - " + err);
+      synthesizer.close();
+      synthesizer = null;
+    });
+    console.log("Now synthesizing to: " + audioFile);
+    })
 
-    // The language of the voice that speaks.
-    speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural"; 
-
-    // Create the speech synthesizer.
-    var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
-
-    synthesizer.speakTextAsync(text,
-      function (result) {
-    if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-      console.log("synthesis finished.");
-    } else {
-      console.error("Speech synthesis canceled, " + result.errorDetails +
-          "\nDid you set the speech resource key and region values?");
-    }
-    synthesizer.close();
-    synthesizer = null;
-  },
-      function (err) {
-    console.trace("err - " + err);
-    synthesizer.close();
-    synthesizer = null;
-  });
-  console.log("Now synthesizing to: " + audioFile);
 };
 
 module.exports = {
