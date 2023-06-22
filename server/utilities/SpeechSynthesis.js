bb921dd990d88e2e7534cb04232d25c79294a35e
Austin <ataraxia@Austins-MacBook-Air.local>
2023-06-22 15:28:12 -0300

    Removed microsoft default settings to allow request to take in text parameter from FE.

--- server/utilities/SpeechSynthesis.js
+++ server/utilities/SpeechSynthesis.js
@@ -19,32 +19,23 @@ const synthesizeSpeech = (text) => {
     // Create the speech synthesizer.
     var synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
 
-    var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-
-    rl.question("Enter some text that you want to speak >\n> ", function (text) {
-      rl.close();
-      // Start the synthesizer and wait for a result.
-      synthesizer.speakTextAsync(text,
-          function (result) {
-        if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
-          console.log("synthesis finished.");
-        } else {
-          console.error("Speech synthesis canceled, " + result.errorDetails +
-              "\nDid you set the speech resource key and region values?");
-        }
-        synthesizer.close();
-        synthesizer = null;
-      },
-          function (err) {
-        console.trace("err - " + err);
-        synthesizer.close();
-        synthesizer = null;
-      });
-      console.log("Now synthesizing to: " + audioFile);
-    });
+    synthesizer.speakTextAsync(text,
+      function (result) {
+    if (result.reason === sdk.ResultReason.SynthesizingAudioCompleted) {
+      console.log("synthesis finished.");
+    } else {
+      console.error("Speech synthesis canceled, " + result.errorDetails +
+          "\nDid you set the speech resource key and region values?");
+    }
+    synthesizer.close();
+    synthesizer = null;
+  },
+      function (err) {
+    console.trace("err - " + err);
+    synthesizer.close();
+    synthesizer = null;
+  });
+  console.log("Now synthesizing to: " + audioFile);
 };
 
 module.exports = {
