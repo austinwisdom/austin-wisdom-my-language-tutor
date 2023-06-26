a993f13fa99e8058756ea3d1e2353e0aa3ecb655
Austin <ataraxia@Austins-MacBook-Air.local>
2023-06-26 14:04:52 -0300

    Styled homepage.

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -8,17 +8,24 @@ const Homepage = () => {
     <>
       <main>
         <section className="home__container">
-          <article className="home__stats">
-            <div>
-              <h2>7 day streak!</h2>
+          <article className="home__stats home__stats--progress">
+            <h1 className="home__heading">Smitty Werbenjägermanjensen</h1>   
+            <div className="challenge__div challenge__div--progress">
+              <h2>Leaderboard Rank: <h2 className="number-one">#1</h2> </h2>
+              <h2>Favorite languages: 🇩🇪 🇺🇲 🇧🇷</h2>
+              <h2>230,326 words spoken</h2>
+              <h2>🔥 976 day streak!</h2>
             </div>
-            <div>
-              <h2>2,326 words spoken</h2>
-            </div>
+          
           </article>
           <article className="home__stats">
-            <div>
-              <h2>Daily Challenges</h2>
+            <div className="challenge__div">
+              <h2 className="challenge__heading">Daily Challenges</h2>
+              <ul className="challenge__ul">
+                <li className="challenge__li"> ✅ Complete 3 conversations</li>
+                <li className="challenge__li">Speak 300 words: 196/300 </li>
+                <li className="challenge__li">✅ Complete 1 conversation in 2 different languages</li>
+              </ul>
             </div>
           </article>
         </section>
